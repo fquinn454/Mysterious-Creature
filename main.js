208e1b82f086c5a4e568cc3d21faa70418488a9d
fquinn454 <57137659+fquinn454@users.noreply.github.com>
2023-01-22 20:00:26 +0000

    Added complementaryDNA

--- main.js
+++ main.js
@@ -64,8 +64,7 @@ function createSampleArr(){
   let specimenNum = 1;
   const sample = [];
   while(sample.length < 30){
-    dna = mockUpStrand();
-    let individual = pAequorFactory(specimenNum, dna);
+    let individual = pAequorFactory(specimenNum, mockUpStrand());
     if(individual.willLikelySurvive()){
       sample.push(individual);
       specimenNum++;
@@ -74,20 +73,58 @@ function createSampleArr(){
   return sample;
 }
 
+// EXTENSTION return complementary DNA strand
+function complementaryDna(pAequor){
+  let complementaryStrand = [];
+  let dnaStrand = pAequor.dna;
+  for(i = 0; i < pAequor.dna.length; i++){
+    switch(dnaStrand[i]){
+      case 'A' : complementaryStrand.push('T');
+        break;
+      case 'T' : complementaryStrand.push('A');
+        break;
+      case 'G' : complementaryStrand.push('C');
+        break;
+      case 'C' : complementaryStrand.push('G');
+        break;
+      default :
+        console.log('Error in DNA sequence');
+        break;
+    }
+  }
+  return complementaryStrand;
+}
+
+// EXTENSION return the two most related organisms in sample array
+function mostRelated(sampleArr){
+  let firstIndividual;
+  let secondIndividual;
+  let percentageRelated = 0;
+  for (i = 0; i <= 30; i++){
+    for(j = 0; j <= 30; j++){
+      if(sampleArr[i] !== sampleArr[j] && sampleArr[i].compareDNA(sampleArr[j]) > percentageRelated){
+        firstIndividual = sampleArr[i];
+        secondIndividual = sampleArr[j];
+        percentageRelated = sampleArr[i].compareDNA(sampleArr[j]); 
+      }
+    }
+  }   
+  console.lot(`Speciment #${firstIndividual._specimenNumber} and Specimen #${secondIndividual._specimenNumber} are most closely
+  related in this sample. The share ${percentageRelated} DNA`);
+
+}
 
 
 //Tests
 dna1 = mockUpStrand();
 dna2 = mockUpStrand();
-console.log(dna1);
-console.log(dna2);
+sample = createSampleArr();
 p1 = pAequorFactory(1, dna1);
-p2 = pAequorFactory(2, dna2);
-p1.compareDNA(p2);
-console.log(p1.willLikelySurvive());
-console.log(p2.willLikelySurvive());
-console.log(createSampleArr());
-
+console.log(p1.dna);
+console.log(complementaryDna(p1));
+console.log(sample)
+console.log(sample[0].compareDNA(sample[1]));
+mostRelated(sample);
 
 
 
