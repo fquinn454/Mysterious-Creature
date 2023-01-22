3a34e8392962092ce667614c136a592e7c8bb3ec
fquinn454 <57137659+fquinn454@users.noreply.github.com>
2023-01-22 18:54:55 +0000

    Added create sample function

--- main.js
+++ main.js
@@ -13,16 +13,11 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
-
-/* Each P.aequor individual has a unique specimen number
-this should not be changed or reset.
-*/
-let _individualCount = 1; 
-
 // Factory Function to create new P.aequor individuals
-function pAequorFactory(_individualCount, arr){
+
+function pAequorFactory(num, arr){
   return {
-    _specimenNumber: _individualCount += 1, 
+    _specimenNumber: num,
     dna: arr,
     mutate(){
       // Generate random number between 0 - 14
@@ -33,9 +28,66 @@ function pAequorFactory(_individualCount, arr){
       while(this.dna[baseNumber ] === baseToChange){
         this.dna[baseNumber] = returnRandBase();
       }
-    }
+    },
+    compareDNA(pAequor){
+      let sameBaseCount = 0;
+      for(i = 0; i < pAequor.dna.length; i++){
+        if(this.dna[i] === pAequor.dna[i]){
+          sameBaseCount++;
+        }
+      }
+      let percentageSimilarity = Math.round(sameBaseCount/pAequor.dna.length * 100);
+      console.log(`Specimen #${this._specimenNumber} and Specimen #${pAequor._specimenNumber} have ${percentageSimilarity}% DNA in common`);
+    }, 
+    willLikelySurvive(){
+      let countCAndG = 0;
+      for(i = 0; i < this.dna.length; i++){
+        if(this.dna[i] === 'G' || this.dna[i] === 'C'){
+          countCAndG++;
+        }
+      }
+      if(Math.floor((countCAndG/this.dna.length)*100 >= 60)){
+        return true;
+      }
+      else{
+        return false;
+      }
+    },
   };
 };
+
+/*
+Create an array containing 30 P.Aequor individuals
+who are likely to survive (60% or more C/G bases)
+*/
+function createSampleArr(){
+  let specimenNum = 1;
+  const sample = [];
+  while(sample.length < 30){
+    dna = mockUpStrand();
+    let individual = pAequorFactory(specimenNum, dna);
+    if(individual.willLikelySurvive()){
+      sample.push(individual);
+      specimenNum++;
+    }
+  }
+  return sample;
+}
+
+
+
+//Tests
+
+dna1 = mockUpStrand();
+dna2 = mockUpStrand();
+console.log(dna1);
+console.log(dna2);
+p1 = pAequorFactory(1, dna1);
+p2 = pAequorFactory(2, dna2);
+p1.compareDNA(p2);
+console.log(p1.willLikelySurvive());
+console.log(p2.willLikelySurvive());
+console.log(createSampleArr());
 
 
 
